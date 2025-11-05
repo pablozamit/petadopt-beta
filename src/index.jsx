0fee616517c9459b6ce43848935032eb8e756bac
pablozamit <166626152+pablozamit@users.noreply.github.com>
2025-11-05 06:21:46 +0100

    Import firebaseConfig in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/tailwind.css";
 import "./styles/index.css";
+import "./firebaseConfig.js"; // <-- AÑADIR ESTA LÍNEA
 
 const container = document.getElementById("root");
 const root = createRoot(container);
