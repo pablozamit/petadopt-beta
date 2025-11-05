34d4c255dee00539f5fff1000bfe4455fcc58fd7
pablozamit <166626152+pablozamit@users.noreply.github.com>
2025-11-05 06:57:38 +0100

    Wrap App component with AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Routes from "./Routes";
 import CookieConsent from "./components/ui/CookieConsent";
 import CookieSettingsButton from "./components/ui/CookieSettingsButton";
+import { AuthProvider } from "./hooks/useAuth";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Routes />
       <CookieConsent />
       <CookieSettingsButton />
-    </>
+    </AuthProvider>
   );
 }
 
